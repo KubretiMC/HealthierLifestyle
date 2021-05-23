8d5c5641b26b1d2efed3f3aa0adf2173eee60d02
KubretiMC <51160512+MCKubreti@users.noreply.github.com>
2021-05-23 18:39:42 +0300

    fixed things about mainactivity menu

--- app/src/main/java/com/mariyan/healthierlifestyle/CustomAdapter.java
+++ app/src/main/java/com/mariyan/healthierlifestyle/CustomAdapter.java
@@ -7,19 +7,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomAdapter extends BaseAdapter {
-    Context context;
+public class CustomAdapter extends BaseAdapter implements Filterable {
 
-    List<RowItem> rowItems;
+    private List<RowItem> rowItems;
+    private List<RowItem> rowItemsListFiltered;
+    private Context context;
 
-    CustomAdapter(Context context, List<RowItem> rowItems){
-        this.context=context;
-        this.rowItems=rowItems;
+    CustomAdapter(List<RowItem> rowItems, Context context) {
+        this.rowItems = rowItems;
+        this.rowItemsListFiltered=rowItems;
+        this.context = context;
     }
 
 
@@ -44,24 +49,24 @@ public class CustomAdapter extends BaseAdapter {
 
         LayoutInflater mInflater = (LayoutInflater) context
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        if(convertView == null) {
-            convertView=mInflater.inflate(R.layout.row_items,null);
-            holder=new ViewHolder();
-            holder.food_name=(TextView) convertView
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.row_items, null);
+            holder = new ViewHolder();
+            holder.food_name = (TextView) convertView
                     .findViewById(R.id.itemName2);
-            holder.food_pic=(ImageView) convertView
+            holder.food_pic = (ImageView) convertView
                     .findViewById(R.id.imageView);
-            holder.food_proteins=(TextView) convertView
+            holder.food_proteins = (TextView) convertView
                     .findViewById(R.id.itemProteins2);
-            holder.food_carbohydrates=(TextView) convertView
+            holder.food_carbohydrates = (TextView) convertView
                     .findViewById(R.id.itemCarbohydrates2);
-            holder.food_fats=(TextView) convertView
+            holder.food_fats = (TextView) convertView
                     .findViewById(R.id.itemFats2);
-            holder.food_calories=(TextView) convertView
+            holder.food_calories = (TextView) convertView
                     .findViewById(R.id.itemCalories2);
 
 
-            RowItem row_pos= rowItems.get(position);
+            RowItem row_pos = rowItems.get(position);
 
             holder.food_pic.setImageResource(row_pos.getFood_pic_id());
             holder.food_name.setText(row_pos.getFood_name());
@@ -74,7 +79,49 @@ public class CustomAdapter extends BaseAdapter {
         return convertView;
     }
 
-    private class ViewHolder{
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                FilterResults filterResults = new FilterResults();
+                if (constraint == null || constraint.length() == 0) {
+                    filterResults.count = rowItems.size();
+                    filterResults.values = rowItems;
+
+                } else {
+                    List<RowItem> resultsModel = new ArrayList<>();
+                    String searchStr = constraint.toString().toLowerCase();
+
+                    for (RowItem rowItem : rowItems) {
+                        if (rowItem.getFood_name().contains(searchStr)) {
+                            resultsModel.add(rowItem);
+                            filterResults.count = resultsModel.size();
+                            filterResults.values = resultsModel;
+                        }
+                    }
+
+
+                }
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                rowItemsListFiltered = (List<RowItem>) results.values;
+                notifyDataSetChanged();
+
+            }
+        };
+        return filter;
+    }
+
+
+
+    private class ViewHolder {
         ImageView food_pic;
         TextView food_name;
         TextView food_proteins;
--- app/src/main/java/com/mariyan/healthierlifestyle/FoodsListActivity.java
+++ app/src/main/java/com/mariyan/healthierlifestyle/FoodsListActivity.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,8 +17,10 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
@@ -28,7 +33,8 @@ import java.util.List;
 import java.util.Objects;
 
 
-public class FoodsListActivity extends Activity implements AdapterView.OnItemClickListener {
+
+public class FoodsListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     Button load;
     Connection connect;
     String ConnectionResult = "";
@@ -40,7 +46,7 @@ public class FoodsListActivity extends Activity implements AdapterView.OnItemCli
     ListView mylistview;
 
     Context context = this;
-
+    CustomAdapter adapter;
     //@RequiresApi(api = Build.VERSION_CODES.N)
     //@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,12 @@ public class FoodsListActivity extends Activity implements AdapterView.OnItemCli
 
 
         loadImageByInternetURL();
+
+        mylistview = (ListView) findViewById(R.id.simpleListView);
+        adapter = new CustomAdapter(rowItems,this);
+        mylistview.setAdapter(adapter);
+
+        mylistview.setOnItemClickListener(this);
 
 //        rowItems = new ArrayList<RowItem>();
 //        food_names = getResources().getStringArray(R.array.Foods_names);
@@ -96,11 +108,7 @@ public class FoodsListActivity extends Activity implements AdapterView.OnItemCli
                             rs.getFloat(3),rs.getFloat(4),  rs.getFloat(5),rs.getFloat(6));
                     rowItems.add(item);
                 }
-                mylistview = (ListView) findViewById(R.id.simpleListView);
-                CustomAdapter adapter = new CustomAdapter(this,rowItems);
-                mylistview.setAdapter(adapter);
 
-                mylistview.setOnItemClickListener(this);
                 return mylistview;
 
 
@@ -136,5 +144,49 @@ public class FoodsListActivity extends Activity implements AdapterView.OnItemCli
                 .with(context)
                 .load(internetUrl)
                 .into(targetImageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search_view);
+
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                Log.e("Main"," data search"+newText);
+
+                adapter.getFilter().filter(newText);
+
+
+
+                return true;
+            }
+        });
+
+
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.search_view){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
